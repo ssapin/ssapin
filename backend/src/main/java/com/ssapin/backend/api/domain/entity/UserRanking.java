e8bb3c4b77aa41987c7664e41d12357707ebe08f
SSAFY <jiwon0297@naver.com>
2022-10-24 10:54:54 +0900

    :sparkles: Feat: UserRanking Entity 생성

--- backend/src/main/java/com/ssapin/backend/api/domain/entity/UserRanking.java
+++ backend/src/main/java/com/ssapin/backend/api/domain/entity/UserRanking.java
@@ -1,4 +1,30 @@
 package com.ssapin.backend.api.domain.entity;
 
-public class UserRanking {
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicInsert;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Getter
+@DynamicInsert
+@NoArgsConstructor
+@Table(name="user_ranking")
+public class UserRanking extends BaseEntity {
+    @Column(nullable = false)
+    private long userId;
+
+    public UserRanking update(long userId){
+        this.userId = userId;
+        return this;
+    }
+
+    @Builder
+    public UserRanking(long userId){
+        this.userId = userId;
+    }
 }
