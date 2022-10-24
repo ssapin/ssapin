b0fa89e89b84f31f3367f1532544b206083a918d
SSAFY <jiwon0297@naver.com>
2022-10-24 11:10:55 +0900

    :sparkles: Map Entity 생성

--- backend/src/main/java/com/ssapin/backend/api/domain/entity/Map.java
+++ backend/src/main/java/com/ssapin/backend/api/domain/entity/Map.java
@@ -1,4 +1,49 @@
 package com.ssapin.backend.api.domain.entity;
 
-public class Map {
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicInsert;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Getter
+@DynamicInsert
+@NoArgsConstructor
+@Table(name="map")
+public class Map extends BaseEntity {
+    @Column(nullable = false)
+    private long campusId;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private boolean access;
+
+    @Column(nullable = false)
+    private long userId;
+
+    @Column(nullable = false)
+    private String emoji;
+
+    public Map update(long campusId, String title, boolean access, String emoji){
+        this.campusId=campusId;
+        this.title=title;
+        this.access=access;
+        this.emoji=emoji;
+        return this;
+    }
+
+    @Builder
+    public Map(long campusId, String title, boolean access, long userId, String emoji){
+        this.campusId=campusId;
+        this.title=title;
+        this.access=access;
+        this.userId=userId;
+        this.emoji=emoji;
+    }
 }
