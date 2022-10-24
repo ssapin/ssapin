bffbb3717177b6b2f69da3e9d63537a84da21982
SSAFY <jiwon0297@naver.com>
2022-10-24 12:22:23 +0900

    :sparkles: MapRanking Entity 생성

--- backend/src/main/java/com/ssapin/backend/api/domain/entity/MapRanking.java
+++ backend/src/main/java/com/ssapin/backend/api/domain/entity/MapRanking.java
@@ -1,4 +1,30 @@
 package com.ssapin.backend.api.domain.entity;
 
-public class MapRanking {
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicInsert;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Getter
+@DynamicInsert
+@NoArgsConstructor
+@Table(name="map_ranking")
+public class MapRanking extends BaseEntity {
+    @Column(nullable = false)
+    private long mapId;
+
+    public MapRanking update(long mapId){
+        this.mapId = mapId;
+        return this;
+    }
+
+    @Builder
+    public MapRanking(long mapId){
+        this.mapId = mapId;
+    }
 }
