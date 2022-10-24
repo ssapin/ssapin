491367eb5d1904179ff879bfa703efd09064f851
SSAFY <jiwon0297@naver.com>
2022-10-24 12:25:24 +0900

    :sparkles: Togethermap Entity 생성

--- backend/src/main/java/com/ssapin/backend/api/domain/entity/Togethermap.java
+++ backend/src/main/java/com/ssapin/backend/api/domain/entity/Togethermap.java
@@ -1,4 +1,35 @@
 package com.ssapin.backend.api.domain.entity;
 
-public class Togethermap {
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicInsert;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Getter
+@DynamicInsert
+@NoArgsConstructor
+@Table(name="togethermap")
+public class Togethermap extends BaseEntity {
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private long campusId;
+
+    public Togethermap update(String title, long campusId){
+        this.title=title;
+        this.campusId=campusId;
+        return this;
+    }
+
+    @Builder
+    public Togethermap(String title, long campusId){
+        this.title=title;
+        this.campusId=campusId;
+    }
 }
