e3a64b74fe308fa032b9578255b74f6e3deef0b1
SSAFY <jiwon0297@naver.com>
2022-10-24 11:08:17 +0900

    :sparkles: MapHashTag Entity 생성

--- backend/src/main/java/com/ssapin/backend/api/domain/entity/MapHashtag.java
+++ backend/src/main/java/com/ssapin/backend/api/domain/entity/MapHashtag.java
@@ -1,4 +1,34 @@
 package com.ssapin.backend.api.domain.entity;
 
-public class MapHashtag {
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicInsert;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Getter
+@DynamicInsert
+@NoArgsConstructor
+@Table(name="map_hashtag")
+public class MapHashtag extends BaseEntity {
+    @Column(nullable = false)
+    private long hashtagId;
+
+    @Column(nullable = false)
+    private long mapId;
+
+    public MapHashtag update(long hashtagId){
+        this.hashtagId = hashtagId;
+        return this;
+    }
+
+    @Builder
+    public MapHashtag(long hashtagId, long mapId){
+        this.hashtagId = hashtagId;
+        this.mapId = mapId;
+    }
 }
