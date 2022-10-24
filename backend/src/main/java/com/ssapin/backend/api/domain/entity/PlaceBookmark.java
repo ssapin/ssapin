a8b950c779e5add038e0debf7b6d7e7043cd89ef
SSAFY <jiwon0297@naver.com>
2022-10-24 10:57:19 +0900

    :sparkles: PlaceBookmark Entity 생성

--- backend/src/main/java/com/ssapin/backend/api/domain/entity/PlaceBookmark.java
+++ backend/src/main/java/com/ssapin/backend/api/domain/entity/PlaceBookmark.java
@@ -1,4 +1,36 @@
 package com.ssapin.backend.api.domain.entity;
 
-public class PlaceBookmark {
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicInsert;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Getter
+@DynamicInsert
+@NoArgsConstructor
+@Table(name="place_bookmark")
+public class PlaceBookmark extends BaseEntity {
+    @Column(nullable = false)
+    private long userId;
+
+    @Column(nullable = false)
+    private long placeId;
+
+    public PlaceBookmark update(long userId, long placeId){
+        this.userId = userId;
+        this.placeId = placeId;
+        return this;
+    }
+
+    @Builder
+    public PlaceBookmark(long userId, long placeId){
+
+        this.userId = userId;
+        this.placeId = placeId;
+    }
 }
