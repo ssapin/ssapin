e3e0580a71b4fc6cba28b4e46a5b815ccf4647e1
SSAFY <jiwon0297@naver.com>
2022-10-24 11:12:33 +0900

    :sparkles: MapPlace Entity 생성

--- backend/src/main/java/com/ssapin/backend/api/domain/entity/MapPlace.java
+++ backend/src/main/java/com/ssapin/backend/api/domain/entity/MapPlace.java
@@ -1,4 +1,39 @@
 package com.ssapin.backend.api.domain.entity;
 
-public class MapPlace {
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicInsert;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Getter
+@DynamicInsert
+@NoArgsConstructor
+@Table(name="map_place")
+public class MapPlace extends BaseEntity {
+    @Column(nullable = false)
+    private long mapId;
+
+    @Column(nullable = false)
+    private long placeId;
+
+    @Column(nullable = false)
+    private long userId;
+
+    public MapPlace update(long mapId, long placeId){
+        this.mapId = mapId;
+        this.placeId = placeId;
+        return this;
+    }
+
+    @Builder
+    public MapPlace(long mapId, long placeId, long userId){
+        this.mapId = mapId;
+        this.placeId = placeId;
+        this.userId = userId;
+    }
 }
