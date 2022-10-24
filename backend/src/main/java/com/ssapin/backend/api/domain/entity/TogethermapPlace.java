7a03c974eea132f0d5454e6039376cc914cab50e
SSAFY <jiwon0297@naver.com>
2022-10-24 12:28:56 +0900

    :sparkles: TogethermapPlace Entity 생성

--- backend/src/main/java/com/ssapin/backend/api/domain/entity/TogethermapPlace.java
+++ backend/src/main/java/com/ssapin/backend/api/domain/entity/TogethermapPlace.java
@@ -1,5 +1,39 @@
 package com.ssapin.backend.api.domain.entity;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicInsert;
 
-public class TogethermapPlace {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Getter
+@DynamicInsert
+@NoArgsConstructor
+@Table(name="togethermap_place")
+public class TogethermapPlace extends BaseEntity {
+    @Column(nullable = false)
+    private long placeId;
+
+    @Column(nullable = false)
+    private long togethermapId;
+
+    @Column(nullable = false)
+    private long userId;
+
+    public TogethermapPlace update(long placeId, long togethermapId){
+        this.placeId=placeId;
+        this.togethermapId=togethermapId;
+        return this;
+    }
+
+    @Builder
+    public TogethermapPlace(long placeId, long togethermapId, long userId){
+        this.placeId=placeId;
+        this.togethermapId=togethermapId;
+        this.userId=userId;
+    }
 }
