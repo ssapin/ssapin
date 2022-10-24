0543250becf3b8e34d0ffa480e36f34a26f88032
SSAFY <jiwon0297@naver.com>
2022-10-24 11:02:03 +0900

    :sparkles: MapBookmark Entity 생성

--- backend/src/main/java/com/ssapin/backend/api/domain/entity/MapBookmark.java
+++ backend/src/main/java/com/ssapin/backend/api/domain/entity/MapBookmark.java
@@ -1,4 +1,35 @@
 package com.ssapin.backend.api.domain.entity;
 
-public class MapBookmark {
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicInsert;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Getter
+@DynamicInsert
+@NoArgsConstructor
+@Table(name="map_bookmark")
+public class MapBookmark extends BaseEntity {
+    @Column(nullable = false)
+    private long userId;
+
+    @Column(nullable = false)
+    private long mapId;
+
+    public MapBookmark update(long userId, long mapId){
+        this.userId = userId;
+        this.mapId = mapId;
+        return this;
+    }
+
+    @Builder
+    public MapBookmark(long userId, long mapId){
+        this.userId = userId;
+        this.mapId = mapId;
+    }
 }
