6fbfb8f9cf8d2ccb4114110765dc02a45e2c1246
SSAFY <jiwon0297@naver.com>
2022-10-24 11:06:04 +0900

    :sparkles: HashTag Entity 생성

--- backend/src/main/java/com/ssapin/backend/api/domain/entity/Hashtag.java
+++ backend/src/main/java/com/ssapin/backend/api/domain/entity/Hashtag.java
@@ -1,4 +1,30 @@
 package com.ssapin.backend.api.domain.entity;
 
-public class Hashtag {
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicInsert;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Getter
+@DynamicInsert
+@NoArgsConstructor
+@Table(name="hashtag")
+public class Hashtag extends BaseEntity {
+    @Column(nullable = false)
+    private String content;
+
+    public Hashtag update(String content){
+        this.content = content;
+        return this;
+    }
+
+    @Builder
+    public Hashtag(String content){
+        this.content = content;
+    }
 }
