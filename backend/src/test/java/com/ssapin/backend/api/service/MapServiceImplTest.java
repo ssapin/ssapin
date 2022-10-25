5b4c4020bca484914c56614cf75d02c410df48bb
SSAFY <jiwon0297@naver.com>
2022-10-25 16:00:16 +0900

    :white_check_mark: Test: Map 수정 API 테스트 코드 작성 완료

--- backend/src/test/java/com/ssapin/backend/api/service/MapServiceImplTest.java
+++ backend/src/test/java/com/ssapin/backend/api/service/MapServiceImplTest.java
@@ -2,14 +2,12 @@ package com.ssapin.backend.api.service;
 
 import com.ssapin.backend.api.domain.dto.request.HashtagRequest;
 import com.ssapin.backend.api.domain.dto.request.MapRequest;
-import com.ssapin.backend.api.domain.entity.Campus;
-import com.ssapin.backend.api.domain.entity.Hashtag;
-import com.ssapin.backend.api.domain.entity.Map;
-import com.ssapin.backend.api.domain.entity.User;
+import com.ssapin.backend.api.domain.entity.*;
 import com.ssapin.backend.api.domain.repository.CampusRepository;
 import com.ssapin.backend.api.domain.repository.HashtagRepository;
 import com.ssapin.backend.api.domain.repository.MapHashtagRepository;
 import com.ssapin.backend.api.domain.repository.MapRepository;
+import com.ssapin.backend.api.domain.repositorysupport.MapHashtagRepositorySupport;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -22,12 +20,9 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 class MapServiceImplTest {
@@ -48,7 +43,7 @@ class MapServiceImplTest {
     private CampusRepository campusRepository;
 
     @Mock
-    Map map;
+    private MapHashtagRepositorySupport mapHashtagRepositorySupport;
 
     @DisplayName("추천지도 생성 테스트")
     @Test
@@ -96,5 +91,75 @@ class MapServiceImplTest {
         //then
         Map findMap = mapRepository.findById(newMapId).get();
         assertEquals(testmap, findMap);
+    }
+
+    @DisplayName("추천지도 생성 테스트")
+    @Test
+    void updateMap() throws Exception {
+        //given
+        List<HashtagRequest> hashtagList = new ArrayList<>();
+        HashtagRequest hashtag = new HashtagRequest(1);
+        Hashtag originHashtag = Hashtag.builder()
+                .content("test hashtag")
+                .build();
+        hashtagList.add(hashtag);
+
+        createMap();
+        Map originMap = mapRepository.findById(1L).get();
+        List<MapHashtag> originList = new ArrayList<>();
+        MapHashtag originmapHashtag = MapHashtag.builder()
+                .map(originMap)
+                .hashtag(originHashtag)
+                .build();
+        originList.add(originmapHashtag);
+
+        Campus testcampus = Campus.builder()
+                .region("test campus2")
+                .build();
+        User testuser = User.builder()
+                .token("test token2")
+                .nickname("test nickname2")
+                .emoji("test emoji2")
+                .campus(testcampus)
+                .build();
+        Map testmap = Map.builder()
+                .title("test map2")
+                .emoji("test emoji2")
+                .campus(testcampus)
+                .access(true)
+                .user(testuser)
+                .build();
+
+        List<HashtagRequest> edithashtagList = new ArrayList<>();
+        HashtagRequest newhashtag = new HashtagRequest(2);
+        Hashtag testhashtag = Hashtag.builder()
+                .content("test hashtag2")
+                .build();
+        edithashtagList.add(newhashtag);
+        MapRequest.MapEdit editRequest = new MapRequest.MapEdit(
+                originMap.getId(), testmap.getCampus().getId(), testmap.getTitle(), testmap.getEmoji(), testmap.isAccess(), edithashtagList
+        );
+        MapHashtag testmapHashtag = MapHashtag.builder()
+                .map(testmap)
+                .hashtag(testhashtag)
+                .build();
+        List<MapHashtag> testMapHashList = new ArrayList<>();
+        testMapHashList.add(testmapHashtag);
+
+        //mocking
+        given(mapRepository.findById(originMap.getId())).willReturn(Optional.ofNullable(originMap));
+        given(hashtagRepository.findById(newhashtag.getHashtagId())).willReturn(Optional.ofNullable(testhashtag));
+        given(mapHashtagRepositorySupport.findByMapAndHashtag(originMap, originHashtag)).willReturn(testmapHashtag);
+        given(mapHashtagRepositorySupport.findAllByMap(originMap)).willReturn(originList);
+        given(hashtagRepository.findById(newhashtag.getHashtagId())).willReturn(Optional.ofNullable(testhashtag));
+
+        //when
+        Long newMapId = mapService.updateMap(editRequest);
+
+        //then
+        Map findMap = mapRepository.findById(newMapId).get();
+        assertEquals(testmap.getCampus().getId(), findMap.getCampus().getId());
+        assertEquals(testmap.getTitle(), findMap.getTitle());
+        assertEquals(testmap.getEmoji(), findMap.getEmoji());
     }
 }
