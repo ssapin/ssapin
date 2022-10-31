7bb231b145842126bbc395baf2fa15cee31e49e7
SANGBIN IM <97656304+Binzify@users.noreply.github.com>
2022-10-31 17:57:08 +0900

    :lipstick: Style: 장소 버튼 수정하기
    - 장소버튼 수정하고 올리기

--- frontend/src/components/Buttons/RatePlaceButton.tsx
+++ frontend/src/components/Buttons/RatePlaceButton.tsx
@@ -5,64 +5,76 @@ import { ReactComponent as GoodEmojiIcon } from "../../assets/svgs/good.svg";
 import { ReactComponent as NormalEmojiIcon } from "../../assets/svgs/normal.svg";
 import { ReactComponent as BadEmojiIcon } from "../../assets/svgs/bad.svg";
 
-const TiedBoxes = styled.div`
+const OpenTag = styled.div`
   display: flex;
-  justify-content: center;
-  align-items: center;
-  .active {
-    background-color: ${(props) => props.theme.colors.lightBlue};
+  flex-wrap: wrap;
+  margin: 10px;
+  font-family: ${(props) => props.theme.fontFamily.paragraph};
+  font-size: ${(props) => props.theme.fontSizes.paragraph};
+  .checkbox input {
+    display: none;
   }
-`;
-
-const WhiteSquareBox = styled.button`
-  width: ${pixelToRem(70)};
-  height: ${pixelToRem(68)};
-  flex-grow: 0;
-  margin: 0 ${pixelToRem(20)}; 0 0;
-  padding: ${pixelToRem(12)};
-  border-radius: ${pixelToRem(15)};
-  box-shadow: 4px 4px 13px 0 rgba(177, 177, 177, 0.6);
-    background-color: ${(props) => props.theme.colors.gray0};
-  .emoji {
-    font-size: ${(props) => props.theme.fontSizes.h1};
-    width: ${pixelToRem(45)};
-    height: ${pixelToRem(45)};
+  .checkbox {
+    display: flex;
+    margin: 0px 5px 5px 0px;
+  }
+  .checkbox_text {
+    display: flex;
+    background-color: ${(props) => props.theme.colors.lightLightBlue};
+    margin-left: 10px;
+    font-family: ${(props) => props.theme.fontFamily.paragraph};
+    font-size: ${(props) => props.theme.fontSizes.paragraph};
+    color: ${(props) => props.theme.colors.gray500};
+    padding: 7px 20px;
+    border-radius: ${pixelToRem(15)};
+    cursor: pointer;
+    &:hover {
+      background-color: ${(props) => props.theme.colors.lightBlue};
+      color: ${(props) => props.theme.colors.gray0};
+    }
+  }
+  .checkbox input:checked + .checkbox_text {
+    color: ${(props) => props.theme.colors.gray0};
+    background-color: ${(props) => props.theme.colors.lightBlue};
+    font-family: ${(props) => props.theme.fontFamily.paragraphbold};
   }
 `;
 
 export default function PlaceRatingButton() {
-  const [ratingPlace, setRatingPlace] = useState("");
+  const [hashTag, setHashTag] = useState([]);
 
-  const ratePlace = [
+  const countPerson = [
     { key: "1", value: "😄", checked: false },
     { key: "2", value: "🙄", checked: false },
     { key: "3", value: "😡", checked: false },
   ];
 
-  const onChangeMenu = (checked: any, item: any) => {
+  const onChangeTag = (checked: any, item: any) => {
     if (checked) {
-      setRatingPlace([item]);
-      console.log(ratingPlace);
+      setHashTag([...hashTag, item]);
+      console.log(hashTag);
     } else if (!checked) {
-      setRatingPlace(ratingPlace.filter((el: any) => el !== item));
-      console.log(ratingPlace);
+      setHashTag(hashTag.filter((el: any) => el !== item));
+      console.log(hashTag);
     }
   };
 
   return (
-    <TiedBoxes>
-      {ratePlace.map((el) => (
-        <WhiteSquareBox
-          key={el.key}
-          value={el.value}
-          onChange={(e) => {
-            onChangeMenu(e.target.checked, e.target.value);
-          }}
-          checked={ratingPlace.includes(el.value)}
-        >
-          <span className="emoji">{el.value}</span>
-        </WhiteSquareBox>
+    <OpenTag>
+      {countPerson.map((el) => (
+        // eslint-disable-next-line jsx-a11y/label-has-associated-control
+        <label className="checkbox" key={el.key}>
+          <input
+            type="checkbox"
+            value={el.value}
+            onChange={(e) => {
+              onChangeTag(e.target.checked, e.target.value);
+            }}
+            checked={hashTag.includes(el.value)}
+          />
+          <span className="checkbox_text">{el.value}</span>
+        </label>
       ))}
-    </TiedBoxes>
+    </OpenTag>
   );
 }
