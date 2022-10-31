3efc0ff7d2918af6dc11928fa4d5e3a164731217
SSAFY <jiwon0297@naver.com>
2022-10-31 17:23:52 +0900

    :bug: Fix: map/togethermap placeCnt, userCnt 추가

--- backend/src/main/java/com/ssapin/backend/api/domain/dto/response/TogethermapResponse.java
+++ backend/src/main/java/com/ssapin/backend/api/domain/dto/response/TogethermapResponse.java
@@ -3,7 +3,9 @@ package com.ssapin.backend.api.domain.dto.response;
 import com.ssapin.backend.api.domain.entity.Togethermap;
 import lombok.Getter;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Getter
 public class TogethermapResponse {
@@ -11,11 +13,17 @@ public class TogethermapResponse {
     private String title;
     private long campusId;
     private List<PlaceResponse> placeList;
+    private long userCnt;
 
     public TogethermapResponse (Togethermap togethermap, List<PlaceResponse> placeList) {
         this.togethermapId = togethermap.getId();
         this.title = togethermap.getTitle();
         this.campusId = togethermap.getCampus().getId();
         this.placeList = placeList;
+        Set<Long> userSet = new HashSet<>();
+        for(PlaceResponse placeResponse : placeList) {
+            userSet.add(placeResponse.getUserId());
+        }
+        this.userCnt = userSet.size();
     }
 }
