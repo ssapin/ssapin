5e6a293952525ed2b9fc6d338cd508335e8372f4
유지연 <wldusdl1023@naver.com>
2022-11-01 10:51:29 +0900

    :pencil2: Etc : RankingResponse class 추가
    그렇다구함

--- backend/src/main/java/com/ssapin/backend/api/service/PlaceService.java
+++ backend/src/main/java/com/ssapin/backend/api/service/PlaceService.java
@@ -1,4 +1,34 @@
 package com.ssapin.backend.api.service;
 
+/**
+ * (1) 추천지도에 장소추가
+ * (2) 모여지도에 장소추가/업데이트
+ * (3) 장소랭킹 리스트
+ * (4) 추천지도에 장소 삭제
+ * (5) 모여지도에 장소 삭제
+ * (6) 장소 정보조회
+ * (7) 해당장소가 추가된 추천지도 리스트 조회
+ * (8) 장소 북마크
+ * (9) 장소 북마크 해제
+ **/
 public interface PlaceService {
+
+    Long addPlaceInMap() ;
+
+    Long addPlaceInTogetherMap();
+
+    void getListPlaceRanking() ;
+
+    void removePlaceInMap();
+
+    void removePlaceInTogetherMap();
+
+    void getPlaceInfo();
+
+    void getMapListInPlace ();
+
+    void registerBookmark();
+
+    void removeBookmark();
+
 }
--- backend/src/test/java/com/ssapin/backend/api/service/PlaceServiceImplTest.java
+++ backend/src/test/java/com/ssapin/backend/api/service/PlaceServiceImplTest.java
@@ -1,12 +1,19 @@
 package com.ssapin.backend.api.service;
 
-import com.ssapin.backend.api.domain.repository.PlaceRepository;
+import com.ssapin.backend.api.domain.entity.*;
+import com.ssapin.backend.api.domain.repository.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 
 @ExtendWith(MockitoExtension.class)
 public class PlaceServiceImplTest {
@@ -17,45 +24,109 @@ public class PlaceServiceImplTest {
     @Mock
     private PlaceRepository placeRepository;
 
-    @DisplayName("추천지도에 장소추가")
+    @Mock
+    private CampusRepository campusRepository;
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private MapRepository mapRepository;
+
+    @Mock
+    private MapPlaceRepository mapPlaceRepository;
+
+
+    @DisplayName("추천지도에 장소추가 테스트 ")
     @Test
-    void addPlaceInMap()
+    void addPlaceInMap() throws Exception
+    {
+
+        //given
+
+        Campus testCampus = Campus.builder()
+                .region("test campus")
+                .build();
+
+        User testUser = User.builder()
+                .token("test token")
+                .nickname("test nickname")
+                .emoji("test emoji")
+                .campus(testCampus)
+                .build();
+
+        Map testMap = Map.builder()
+                .campus(testCampus)
+                .title("test mapTitle")
+                .access(true)
+                .user(testUser)
+                .emoji("test emoji")
+                .build();
+
+        Place testPlace = Place.builder()
+                .itemId(1L)
+                .title("test placeTitle")
+                .lat(0L)
+                .lng(0L)
+                .address("test address")
+                .build();
+
+        MapPlace testMapPlace = MapPlace.builder()
+                .map(testMap)
+                .place(testPlace)
+                .user(testUser)
+                .build();
+
+        Long fakeMapPlaceId =1L;
+        ReflectionTestUtils.setField(testMapPlace,"id",fakeMapPlaceId);
+        // ReflectionTestUtils.setField(대상 객체, "변수명", 원하는 값);
+
+        //mocking
+        given(campusRepository.findById(any())).willReturn(Optional.ofNullable(testCampus));
+        given(mapRepository.findById(any())).willReturn(Optional.ofNullable(testMap));
+        given(placeRepository.findById(any())).willReturn(Optional.ofNullable(testPlace));
+
+
+    }
+
+    @DisplayName("모여지도에 장소 추가 /업데이트 테스트")
+    @Test
+    void addPlaceInTogetherMap() throws Exception{}
+
+    @DisplayName("장소랭킹 리스트 테스트")
+    @Test
+    void getListPlaceRanking() throws Exception
     {
 
     }
 
-    @DisplayName("모여지도에 장소 추가 /업데이트")
+
+    @DisplayName("추천 지도에 장소 삭제 테스트")
     @Test
-    void addPlaceInTogethemap(){}
+    void removePlaceInMap() throws Exception
+    {
 
-    @DisplayName("장소랭킹 리스트")
+    }
+
+    @DisplayName("모여지도에서 장소 삭제 테스트")
     @Test
-    void getListPlaceRanking(){}
+    void removePlaceInTogetherMap() throws Exception{}
 
 
-    @DisplayName("추천 지도에 장소 삭제")
+    @DisplayName("장소 상세 정보 조회 테스트")
     @Test
-    void removePlaceInMap(){}
+    void getPlaceInfo()throws Exception{}
 
-    @DisplayName("모여지도에서 장소 삭제")
+    @DisplayName("해당장소가 추가된 추천지도 리스트 조회 테스트")
     @Test
-    void removePlaceInTogetherMap(){}
-
-
-    @DisplayName("장소 상세 정보 조회")
-    @Test
-    void getPlaceInfo(){}
-
-    @DisplayName("해당장소가 추가된 추천지도 리스트 조회")
-    @Test
-    void getMapListInPlace()
+    void getMapListInPlace ()throws Exception
     {}
 
-    @DisplayName("장소 북마크")
+    @DisplayName("장소 북마크 테스트")
     @Test
-    void registerBookmark(){}
+    void registerBookmark()throws Exception{}
 
-    @DisplayName("장소 북마크 해제")
+    @DisplayName("장소 북마크 해제 테스트")
     @Test
-    void removeBookmark(){}
+    void removeBookmark()throws Exception{}
 }
