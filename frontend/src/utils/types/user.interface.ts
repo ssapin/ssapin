88ab3f458d8cb390cfbeeb3977a55a3cc16e6d8e
SSAFY <jiwon0297@naver.com>
2022-11-01 16:22:34 +0900

    :sparkles: Feat: interface 생성

---
+++ frontend/src/utils/types/user.interface.ts
@@ -0,0 +1,13 @@
+export interface IUser {
+  userId: number;
+  nickname: string;
+  campusId: number;
+  emoji: string;
+}
+
+export interface IUserRanking {
+  userId: number;
+  nickname: string;
+  emoji: string;
+  mapCount: number;
+}
