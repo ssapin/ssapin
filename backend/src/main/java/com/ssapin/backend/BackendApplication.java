35aebf80dad365121d6c6803ac355f029c08cf86
SSAFY <lhk924@gmail.com>
2022-11-03 11:05:34 +0900

    ♻️Refactor : Sout 출력문 제거

--- backend/src/main/java/com/ssapin/backend/BackendApplication.java
+++ backend/src/main/java/com/ssapin/backend/BackendApplication.java
@@ -9,10 +9,7 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 public class BackendApplication {
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(BackendApplication.class, args);
-
-		System.out.println();
 	}
 
 }
