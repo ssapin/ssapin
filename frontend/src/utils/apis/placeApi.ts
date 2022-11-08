3612df81e09b322656cce5afc60ed8b67334d0a3
유지연 <wldusdl1023@naver.com>
2022-11-08 14:30:04 +0900

    :sparkles: Feat : PlaceApi.ts 생성
    util/apis/placeApi.ts

--- frontend/src/utils/apis/placeApi.ts
+++ frontend/src/utils/apis/placeApi.ts
@@ -1,3 +1,10 @@
-export const PLACE_APIS = {
+const PLACE_APIS = {
   getPlaceRanking: (campusId: number) => `/place/ranking/${campusId}`,
+  getDetailPlaceInfo: (itemId: number) => `/place/${itemId}/detail/`,
+  getMapListInMap: (itemId: number) => `/place/map/${itemId}`,
+  PLACE_MAP: `place/login/map`,
+  PLACE_TOGETHERMAP: `place/login/togethermap`,
+  BOOKMARK: `place/login/bookmark`,
 };
+
+export default PLACE_APIS;
