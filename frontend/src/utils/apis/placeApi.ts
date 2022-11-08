e1bca8ce21b554d7029e452393d107e7ceec8abf
유지연 <wldusdl1023@naver.com>
2022-11-08 15:44:07 +0900

    :sparkles: Feat : 장소 관련 axios 파일 생성
    utils/functions/place.ts

--- frontend/src/utils/apis/placeApi.ts
+++ frontend/src/utils/apis/placeApi.ts
@@ -1,9 +1,9 @@
 const PLACE_APIS = {
   getPlaceRanking: (campusId: number) => `/place/ranking/${campusId}`,
   getDetailPlaceInfo: (itemId: number) => `/place/${itemId}/detail/`,
-  getMapListInMap: (itemId: number) => `/place/map/${itemId}`,
-  PLACE_MAP: `place/login/map`,
-  PLACE_TOGETHERMAP: `place/login/togethermap`,
+  getMapListInPlace: (itemId: number) => `/place/map/${itemId}`,
+  MAP: `place/login/map`,
+  TOGETHERMAP: `place/login/togethermap`,
   BOOKMARK: `place/login/bookmark`,
 };
 
--- frontend/src/utils/functions/place.ts
+++ frontend/src/utils/functions/place.ts
@@ -1,1 +1,64 @@
-export const addPlace = () => { }
+import axiosInstance from "../apis/api";
+import PLACE_APIS from "../apis/placeApi";
+import {
+  IAddPlace,
+  IPlaceBookmark,
+  IRemovePlaceMap,
+  IRemovePlaceTogethermap,
+} from "../types/place.interface";
+
+// 추천지도 장소추가
+export function addPlaceToMap(data: IAddPlace) {
+  axiosInstance.post(PLACE_APIS.MAP, data);
+}
+
+// 모여지도 장소추가/업데이트
+export function addPlaceToTogetherMap(data: IAddPlace) {
+  axiosInstance.post(PLACE_APIS.MAP, data);
+}
+
+// 추천지도 장소 삭제
+export function removePlaceInMap(d_data: IRemovePlaceMap) {
+  axiosInstance.delete(PLACE_APIS.MAP, {
+    data: {
+      placeId: d_data.placeId,
+      mapId: d_data.mapId,
+    },
+  });
+}
+
+// 모여지도 장소 삭제
+export function removePlaceInTogethermap(d_data: IRemovePlaceTogethermap) {
+  axiosInstance.delete(PLACE_APIS.TOGETHERMAP, {
+    data: {
+      placeId: d_data.placeId,
+      togethermapId: d_data.togethermapId,
+    },
+  });
+}
+
+// 장소 정보 조회
+export function getDetailPlaceInfo(itemId: String) {
+  const id: number = Number(itemId);
+
+  axiosInstance.get(PLACE_APIS.getDetailPlaceInfo(id));
+}
+
+// 장소가 추가된 추천지도 리스트 조회
+export function getMapListInPlace(itemId: String) {
+  const id: number = Number(itemId);
+
+  axiosInstance.get(PLACE_APIS.getMapListInPlace(id));
+}
+
+// 장소 북마크 등록
+export function addBookmarkInPlace(data: IPlaceBookmark) {
+  axiosInstance.post(PLACE_APIS.BOOKMARK, data);
+}
+
+// 장소 북마크 해제
+export function removeBookmarkInPlace(d_data: IPlaceBookmark) {
+  axiosInstance.delete(PLACE_APIS.BOOKMARK, {
+    data: { itemId: d_data.itemId },
+  });
+}
