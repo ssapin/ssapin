8d3c72f8dafd1d390b7145a1725e990168f71985
jiwon0297 <jiwon0297@naver.com>
2022-11-10 15:06:45 +0900

    :bug: Fix: Place Ranking 수정

--- backend/src/main/java/com/ssapin/backend/api/domain/repository/ReviewRepository.java
+++ backend/src/main/java/com/ssapin/backend/api/domain/repository/ReviewRepository.java
@@ -1,12 +1,19 @@
 package com.ssapin.backend.api.domain.repository;
 
-import com.ssapin.backend.api.domain.entity.Place;
 import com.ssapin.backend.api.domain.entity.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+    @Query(nativeQuery = true,
+            value="select IFNULL(B.id,0) " +
+                    "from review right join " +
+                    "(select distinct(map_place.place_id) as id from map_place right join map on map.id=map_place.map_id and map.campus_id=:campusId order by id) as B " +
+                    "on review.place_id = B.id " +
+                    "group by B.id " +
+                    "order by count(*) desc " +
+                    "limit 1 ")
+    Long makeReviewRanking(long campusId);
 }
