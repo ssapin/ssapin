9c6b838950c704b4d5b9db8ae247decf12dd4d1a
유지연 <wldusdl1023@naver.com>
2022-11-14 22:54:42 +0900

    :pencil2: Etc : 코드정리

--- frontend/src/components/etc/EmojiKeyboard.tsx
+++ frontend/src/components/etc/EmojiKeyboard.tsx
@@ -17,68 +17,68 @@ function EmojiKeyBoard() {
   }
 
   return (
-    <div className="App">
-      <h2>Emoji Picker React 4 Demo</h2>
-      <div className="show-emoji">
-        Your selected Emoji is:
-        {selectedEmoji ? (
-          <Emoji
-            unified={selectedEmoji}
-            emojiStyle={EmojiStyle.NATIVE}
-            size={22}
-          />
-        ) : null}
-      </div>
+    // <div className="App">
+    //   <h2>Emoji Picker React 4 Demo</h2>
+    //   <div className="show-emoji">
+    //     Your selected Emoji is:
+    //     {selectedEmoji ? (
+    //       <Emoji
+    //         unified={selectedEmoji}
+    //         emojiStyle={EmojiStyle.NATIVE}
+    //         size={22}
+    //       />
+    //     ) : null}
+    //   </div>
 
-      <EmojiPicker
-        // eslint-disable-next-line react/jsx-no-bind
-        onEmojiClick={onClick}
-        autoFocusSearch
-        emojiStyle={EmojiStyle.NATIVE}
-        emojiVersion="5.0"
-        theme={Theme.DARK}
-        previewConfig={{
-          defaultCaption: "싸핀러의 이모지는 무엇!",
-          defaultEmoji: "1f92a", // 🤪
-        }}
-        suggestedEmojisMode={SuggestionMode.RECENT}
-        defaultSkinTone={SkinTones.NEUTRAL}
-        categories={[
-          {
-            name: "제안",
-            category: Categories.SUGGESTED,
-          },
-          {
-            name: "표정",
-            category: Categories.SMILEYS_PEOPLE,
-          },
-          {
-            name: "자연&동물",
-            category: Categories.ANIMALS_NATURE,
-          },
-          {
-            name: "음식",
-            category: Categories.FOOD_DRINK,
-          },
-          {
-            name: "여행",
-            category: Categories.TRAVEL_PLACES,
-          },
-          {
-            name: "활동",
-            category: Categories.ACTIVITIES,
-          },
-          {
-            name: "사물",
-            category: Categories.OBJECTS,
-          },
-          {
-            name: "깃발",
-            category: Categories.FLAGS,
-          },
-        ]}
-      />
-    </div>
+    <EmojiPicker
+      // eslint-disable-next-line react/jsx-no-bind
+      onEmojiClick={onClick}
+      autoFocusSearch
+      emojiStyle={EmojiStyle.NATIVE}
+      emojiVersion="5.0"
+      theme={Theme.LIGHT}
+      previewConfig={{
+        defaultCaption: "싸핀러의 이모지는 무엇!",
+        defaultEmoji: "1f92a", // 🤪
+      }}
+      suggestedEmojisMode={SuggestionMode.RECENT}
+      defaultSkinTone={SkinTones.NEUTRAL}
+      categories={[
+        {
+          name: "제안",
+          category: Categories.SUGGESTED,
+        },
+        {
+          name: "표정",
+          category: Categories.SMILEYS_PEOPLE,
+        },
+        {
+          name: "자연&동물",
+          category: Categories.ANIMALS_NATURE,
+        },
+        {
+          name: "음식",
+          category: Categories.FOOD_DRINK,
+        },
+        {
+          name: "여행",
+          category: Categories.TRAVEL_PLACES,
+        },
+        {
+          name: "활동",
+          category: Categories.ACTIVITIES,
+        },
+        {
+          name: "사물",
+          category: Categories.OBJECTS,
+        },
+        {
+          name: "깃발",
+          category: Categories.FLAGS,
+        },
+      ]}
+    />
+    // </div>
   );
 }
 
