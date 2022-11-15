f400060c5f924fa4faaed4149d76d884378f1230
Eunyeol Nam <ney9083@gmail.com>
2022-11-15 17:46:36 +0900

    :sparkles: Feat: 마이페이지 이모지 슬롯머신 기능

--- frontend/src/components/Buttons/EmojiSlotMachine.stories.tsx
+++ frontend/src/components/Buttons/EmojiSlotMachine.stories.tsx
@@ -3,20 +3,20 @@ import { ComponentStory, ComponentMeta } from "@storybook/react";
 import { withRouter } from "storybook-addon-react-router-v6";
 import GlobalStyle from "../../styles/GlobalStyle";
 import themes from "../../styles/theme";
-import Random from "./Random";
+import EmojiSlotMachine from "./EmojiSlotMachine";
 
 export default {
-  title: "Components/Random",
-  component: Random,
+  title: "Components/EmojiSlotMachine",
+  component: EmojiSlotMachine,
   decorators: [withRouter],
-} as ComponentMeta<typeof Random>;
+} as ComponentMeta<typeof EmojiSlotMachine>;
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 
 // eslint-disable-next-line react/function-component-definition
-const Template: ComponentStory<typeof Random> = () => (
+const Template: ComponentStory<typeof EmojiSlotMachine> = () => (
   // eslint-disable-next-line react/jsx-props-no-spreading
-  <Random />
+  <EmojiSlotMachine />
 );
 
 export const RandomButton = Template.bind({});
