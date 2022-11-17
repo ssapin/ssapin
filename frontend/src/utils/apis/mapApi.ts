53da76873cff01e2f809b873475bbb3572e1850d
유지연 <wldusdl1023@naver.com>
2022-11-17 14:44:15 +0900

    :recycle: Refactor : 메인페이지 맵리스트 가져오는 axios 함수 작성

--- frontend/src/utils/apis/mapApi.ts
+++ frontend/src/utils/apis/mapApi.ts
@@ -75,3 +75,14 @@ export const removeMapBookmark = async (rData: IBookMark) => {
     return Promise.reject(error);
   }
 };
+
+export const getMapListInMainpage = async (campusId: number) => {
+  try {
+    const response: AxiosResponse<IMap> = await axiosInstance.get(
+      MAP_APIS.GET_MAP_LIST_MAINPAGE(campusId),
+    );
+    return response.data;
+  } catch (error) {
+    return Promise.reject(error);
+  }
+};
